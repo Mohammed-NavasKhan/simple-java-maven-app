ce3ea8e91e2160a21080ace4263d4f584e6ca5ec
Mohammed NavasKhan <91381698+Mohammed-NavasKhan@users.noreply.github.com>
2024-08-08 09:58:17 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,16 +11,24 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
+echo "Project Name: ${NAME}"
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
+echo "Project Version: ${VERSION}"
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
+echo "Checking for JAR file at target/${NAME}-${VERSION}.jar"
+if [ -f "target/${NAME}-${VERSION}.jar" ]; then
+  echo "JAR file exists, running it."
+  java -jar target/${NAME}-${VERSION}.jar
+else
+  echo "JAR file not found. Please check the file name and build process."
+  exit 1
+fi
